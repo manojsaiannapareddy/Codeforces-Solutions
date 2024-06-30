30109205cea0e21192b12bdeb684779e4cb50aaa
CosmoXGod <amanojs1980@gmail.com>
2024-06-30 19:22:46 +0530

    Codefore Questions

--- borze.py
+++ borze.py
@@ -1,22 +1,18 @@
 def borze():
     a = str(input())
     b = ""
-    if a[0] == ".":
-        b += "0"
-        a = a[1:]
     temp = ""
     for i in a:
-        if len(temp) < 2:
-            temp += i
-        else:
-            if temp == "-.":
-                b += "1"
-                temp = ""
-            elif temp == "--":
-                b += "2"
-                temp = ""
-    if a[0] != "." and len(a) %2 != 0 and a[-1] == ".":
-        b += "."
+        temp += i
+        if temp == ".":
+            b += "0"
+            temp = ""
+        elif temp == "-.":
+            b += "1"
+            temp = ""
+        elif temp == "--":
+            b += "2"
+            temp = ""
     
     return b
 
